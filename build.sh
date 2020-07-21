70fbde0f9b5f25ee7b17aa58b08c3c868279be6a
oldthreefeng <louisehong4168@gmail.com>
2020-07-22 00:24:23 +0800

    a

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # build.sh v3.0.2
 COMMIT_SHA1=$(git rev-parse --short HEAD || echo "0.0.0")
 BUILD_TIME=$(date "+%F %T")
-go build -ldflags "-X github.com/oldhtreefeng/sshgo/cmd.Version=$1 -X github.com/oldhtreefeng/sshgo/cmd.Build=${COMMIT_SHA1} -X 'github.com/oldhtreefeng/sshgo/cmd.BuildTime=${BUILD_TIME}'" sshgo.go
+go build -ldflags "-X github.com/oldthreefeng/sshgo/cmd.Version=$1 -X github.com/oldthreefeng/sshgo/cmd.Build=${COMMIT_SHA1} -X 'github.com/oldthreefeng/sshgo/cmd.BuildTime=${BUILD_TIME}'" sshgo.go
